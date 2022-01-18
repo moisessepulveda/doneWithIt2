8b302bd05390d01f026a128ac51b31356e4e24b9
moisessepulveda <mo.sepulvedar@gmail.com>
2022-01-18 18:12:37 -0300

    welcome screen in progress, background and icon already added

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,9 +1,44 @@
 import React from "react";
+import {View, ImageBackground, StyleSheet, Image} from "react-native";
 
-import {ImageBackground} from "react-native"
-
-function welcomeScreen(props) {
+function WelcomeScreen() {
     return(
-        <ImageBackground source={require('../assets/background.jpg')} />
+        <ImageBackground
+            style={styles.background}
+            source={require('../assets/background.jpg')}>
+            <Image
+                style={styles.logo}
+                source={require('../assets/logo-red.png')}/>
+
+            <View style={styles.loginButton} />
+
+            <View style={styles.registerButton} />
+        </ImageBackground>
     )
 }
+export default WelcomeScreen
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end"
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#fc5c65'
+    },
+    logo: {
+        width: 100,
+        height: 100,
+        top: 50,
+        position: "absolute",
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#4ecdc4'
+    }
+})
+
+
